5099e23f9526e9b136e0f6a9bd59cf26f4d732c5
kisololo <97356094+kisololo@users.noreply.github.com>
2025-11-22 16:31:23 -0700

    Add files via upload

--- MS-app.py
+++ MS-app.py
@@ -2,23 +2,25 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-# ------------------------------
-# Google Analytics (GA4)
-# ------------------------------
-ga_id = "G-7SJTF762GX"  # Your GA4 Measurement ID
+import streamlit.components.v1 as components
 
-ga_script = f"""
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={ga_id}"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){{dataLayer.push(arguments);}}
-  gtag('js', new Date());
-  gtag('config', '{ga_id}');
-</script>
-"""
+#----------------------------------
+# Google Analytics
+#-------------------------------
+components.html(
+    f"""
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7SJTF762GX"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){{dataLayer.push(arguments);}}
+      gtag('js', new Date());
+      gtag('config', 'G-7SJTF762GX');
+    </script>
+    """,
+    height=0,
+)
 
-st.markdown(ga_script, unsafe_allow_html=True)
 
 # ---------------------------------------------------------------------
 # GLOBAL PLOT STYLE
@@ -282,7 +284,7 @@ footer = """
 <style>
 .footerbox {
     width: 100%;
-    margin-top: 50px;
+    margin-top: 30px;
     padding: 25px 0;
     text-align: center;
     color: #bbbbbb;
