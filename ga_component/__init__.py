42d70e8e914a2b5eaad774edcd8f4b596b93593e
kisololo <97356094+kisololo@users.noreply.github.com>
2025-11-23 20:40:44 -0700

    Update __init__.py

--- ga_component/__init__.py
+++ ga_component/__init__.py
@@ -1,12 +1,12 @@
-import os
 import streamlit.components.v1 as components
+import os
 
-# Path to the frontend folder
+# Declare Streamlit component
 _component_func = components.declare_component(
     "ga_inject",
     path=os.path.join(os.path.dirname(__file__), "ga_frontend")
 )
 
 def inject():
-    """Inject the GA4 top-level script."""
+    # We don't need to send data; just initialize component
     return _component_func()
